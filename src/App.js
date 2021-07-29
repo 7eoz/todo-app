1a94bd5cbd5cb3d79675ef74f7f0b4217632f48c
leoSandrini <thesandrini@gmail.com>
2021-07-29 16:53:14 -0300

    Converting project from function to class-based components in order to practice

--- src/App.js
+++ src/App.js
@@ -3,16 +3,24 @@ import TodoItem from './components/TodoItem'
 import todosData from './util/todosData'
 import './styles/index.css'
 
-function App() {
-  const todoComponents = todosData.map(todo => <TodoItem key={todo.id} 
-                                                          text={todo.text} 
-                                                          completed={todo.completed} />)
-  
-  return(
-  <div className="todo-list">
-    {todoComponents}
-  </div>
-  )
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      todoComponents: todosData.map((todo) => 
+                      <TodoItem key={todo.id} 
+                                text={todo.text} 
+                                completed={todo.completed} />
+                      )
+    }
+  }
+  render() {               
+      return(
+      <div className="todo-list">
+        {this.state.todoComponents}
+      </div>
+      )
+    }
 }
 
 export default App
